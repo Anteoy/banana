7427fea81e5102a369a528f20828ce46c373bfad
Anteoy <anteoy@gmail.com>
2018-07-20 00:17:26 +0800

    add velocity

--- src/main/java/com/allocmem/banana/controller/Sample.java
+++ src/main/java/com/allocmem/banana/controller/Sample.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @auther zhoudazhuang
@@ -17,5 +20,13 @@ public class Sample {
   @RequestMapping(value = "/sample", method = RequestMethod.GET)
   public JSONObject clearMixedPushInRedis(String uid) {
     return JSONObject.parseObject("{\"a\":\"b\"}");
+  }
+
+  @RequestMapping({"/velocity_sample" })
+  public ModelAndView test(HttpServletRequest request) {
+    ModelAndView mv =new ModelAndView();
+    mv.addObject("key","hello,velocity！");
+    mv.setViewName("velocity_sample");
+    return mv;
   }
 }
